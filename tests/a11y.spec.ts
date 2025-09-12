7126a3d692f8fb271ea6a12de55c043452183d43
gta3lok <gta3lok.ai@gmail.com>
2025-09-13 01:20:02 +0530

    deleted all tests!

--- tests/a11y.spec.ts
+++
@@ -1,9 +0,0 @@
-// tests/a11y.spec.ts
-import { test, expect } from '@playwright/test';
-import AxeBuilder from '@axe-core/playwright';
-
-test('home has no a11y violations', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  const results = await new AxeBuilder({ page }).analyze();
-  expect(results.violations).toEqual([]);
-});
